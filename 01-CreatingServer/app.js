eb747980f60a46525d2ca53f0e1ba9d6a9c5352d
shubham <shubhamsagar628@gmail.com>
2024-09-29 11:29:05 +0530

    Create server by using http core module

---
+++ 01-CreatingServer/app.js
@@ -0,0 +1,20 @@
+const http = require("node:http")
+
+const server = http.createServer((req,res)=>{
+
+    console.log(req.url)
+    if(req.url === "/api/car"){
+        res.end("this is api of car page")
+    }
+    else if(req.url === "/api/bike"){
+        res.end("This is bike api page")
+    }else{
+        res.end("This is default page")
+    }
+
+})
+
+
+server.listen(4000,(req,res)=>{
+    console.log("Server is listen on Port Number 4000")
+})
